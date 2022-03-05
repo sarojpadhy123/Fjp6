28cf44ce09b8d475a7bf7a9b036bb66837496d61
sarojpadhy123 <psarojkumar9@gmail.com>
2022-03-05 22:15:38 +0530

    commited

--- module2/intro.js
+++ module2/intro.js
@@ -5,3 +5,15 @@ let bool = true;
 for (let i = 1; i <= 5; i++) {
     console.log(i);
 }
+//print num is prime
+let n = 123;
+for (let i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+        isprime = false;
+    }
+}
+if (isprime == true) {
+    console.log("Number is prime");
+} else {
+    console.log("Number is not prime");
+}
